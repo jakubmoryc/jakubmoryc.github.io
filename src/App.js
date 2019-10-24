ef049ae7f065a8bef6cd3adbaab7f8154cbf8a6a
Mortzcent <moryckuba@gmail.com>
2019-10-24 18:51:26 +0200

    navbar todo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import './css/app.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar/>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <div className="navbar">
+                <div className="navbar-logo">
+                    Jakub Moryc
+                </div>
+                <div className="navbar-links">
+                    lol
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
