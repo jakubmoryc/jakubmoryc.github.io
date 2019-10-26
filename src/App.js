ca5372da265e28c4a47bd25250e28df8a0f3550e
Mortzcent <moryckuba@gmail.com>
2019-10-26 17:01:21 +0200

    started navbar; navbar hiding script

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React from 'react';
 import './css/app.css';
-import Navbar from './components/Navbar/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-    </div>
-  );
+import Navbar from './components/Navbar/Navbar';
+import Jumbotron from './components/Jumbotron/Jumbotron';
+
+class App extends React.Component {
+  state = {
+    scrollPos: undefined,
+    showNavbar: true
+  }
+
+  handleScroll = () => {
+    const currentScrollPos = window.pageYOffset
+    if (this.state.scrollPos > currentScrollPos) {
+      this.setState({
+        showNavbar: true
+      })
+    } else {
+      this.setState({
+        showNavbar: false
+      })
+    }
+    this.setState({
+      scrollPos: currentScrollPos
+    })
+  }
+
+  componentDidMount() {
+    this.setState({
+      scrollPos: window.pageYOffset
+    })
+    window.addEventListener('scroll', this.handleScroll)
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar showNavbar={this.state.showNavbar}/>
+        <main>
+          <Jumbotron/>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,23 @@
 import React from 'react'
 
-export default function Navbar() {
+export default function Navbar({ showNavbar }) {
+    const navbarClasses = (showNavbar ? `navbar` : 'navbar hide' )
     return (
         <nav>
-            <div className="navbar">
+            <div 
+                className={navbarClasses}>
                 <div className="navbar-logo">
-                    Jakub Moryc
+                    <h1>JakubMoryc</h1>
                 </div>
                 <div className="navbar-links">
-                    lol
-                    <div>
-
+                    <div className="navbar-links-item">
+                        About
+                    </div>
+                    <div className="navbar-links-item">
+                        My Projects
+                    </div>
+                    <div className="navbar-links-item">
+                        Contact me
                     </div>
                 </div>
             </div>
