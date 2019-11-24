4e85a8c6a34997025409f19eef31fdc4dc5eba84
Mortzcent <moryckuba@gmail.com>
2019-11-24 14:54:32 +0100

    changed project buttons layout

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,19 +7,28 @@ export default function Contact() {
             <p>...and lets work together.</p>
             <div className="contact-section-contacts">
                 <div className="contact-section-contact">
-                    <div className="contact-section-contact-icon">
+                    <div 
+                        className="contact-section-contact-icon" 
+                        onClick={(e) => {window.location.href = "mailto:mail@example.org"}}
+                    >
                         <i class="fas fa-envelope"></i>
                     </div>
                     - <a href="mailto:moryckuba@gmail.com">moryckuba@gmail.com</a>
                 </div>
                 <div className="contact-section-contact">
-                    <div className="contact-section-contact-icon">
+                    <div 
+                        className="contact-section-contact-icon" 
+                        onClick={(e) => {window.location.href = "tel:511-625-146"}}
+                    >
                         <i class="fas fa-phone"></i>
                     </div>
                     - <a href="tel:511-625-146">511 625 146</a>
                 </div>
                 <div className="contact-section-contact">
-                    <div className="contact-section-contact-icon">
+                    <div 
+                        className="contact-section-contact-icon" 
+                        onClick={(e) => {window.open("https://www.linkedin.com/in/jakub-moryc-9446b2198/", "_blank")}}
+                    >
                         <i class="fab fa-linkedin"></i>
                     </div>
                     - <a href="https://www.linkedin.com/in/jakub-moryc-9446b2198/" target="_blank" rel="noopener noreferrer">Jakub Moryc</a>
