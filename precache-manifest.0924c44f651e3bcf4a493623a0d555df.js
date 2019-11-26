7e6295af7fbd1c06e8d73c75572a4a8c56d4519c
Mortzcent <moryckuba@gmail.com>
2019-11-26 16:06:07 +0100

    Updates

---
+++ precache-manifest.0924c44f651e3bcf4a493623a0d555df.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cfa44378651b75390415a29bae04ad0",
+    "url": "/./index.html"
+  },
+  {
+    "revision": "c689a78c0df284807d4f",
+    "url": "/./static/css/main.555e6f2e.chunk.css"
+  },
+  {
+    "revision": "0a8653b4442a07296aac",
+    "url": "/./static/js/2.6f6b9caf.chunk.js"
+  },
+  {
+    "revision": "c689a78c0df284807d4f",
+    "url": "/./static/js/main.66109a11.chunk.js"
+  },
+  {
+    "revision": "1111337c9e37fafbd82c",
+    "url": "/./static/js/runtime-main.a60e84da.js"
+  },
+  {
+    "revision": "2f80e06349fdcfb144dc48a5ce66fe5e",
+    "url": "/./static/media/calculator.2f80e063.png"
+  },
+  {
+    "revision": "af70a646401c9a1f9bddfdfee267c980",
+    "url": "/./static/media/nodejs.af70a646.png"
+  },
+  {
+    "revision": "ccaf101a36a6a9946b007778cffa10cb",
+    "url": "/./static/media/pokedexapp.ccaf101a.png"
+  },
+  {
+    "revision": "b0e928f9619a902cb72aebbe8bd05955",
+    "url": "/./static/media/pokelang-api.b0e928f9.png"
+  }
+]);
